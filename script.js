e0f443e18e4407919447d0c7119977a3ea5fa0e7
Chris de Leon <95385991+chrisdeleon@users.noreply.github.com>
2022-10-10 07:34:24 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+//  Chris de Leon
+//  10/7/2022
+//  CIS131 Midterm Project
+
+var diceArray = [ // array with dice images
+
+
+];
+
+var scoreArray = [ // array with random letter grades
+    "A",
+    "B",
+    "C",
+    "D",
+    "F",
+
+];
+
+var classArray = [ // array with class names, excules CIS260
+    "CIS-101",
+    "CIS-120",
+    "CIS-130",
+    "CIS-131",
+    "CIS-150",
+    "CIS-151",
+    "CIS-210",
+    "CIS-250",
+    "CIS-244",
+    "MTH-140",
+    "MTH-141",
+    "MTH-214",
+    "PLS-101",
+    "PHY-120",
+    "BIO-160"
+];
+
+/*
+
+var container;
+var i = 0; // counter for loops
+var divLoad;
+
+window.addEventListener('load', initializer);
+
+function initializer(){ // initializes page
+    container = document.getElementById('container');
+    boxCreate();
+};
+
+function boxCreate() { // creates 15 box elements
+    for (i = 0; i < 15; i++){
+        divLoad = document.createElement('div');
+        divLoad.innerHTML = classArray[i];
+        container.appendChild(divLoad);
+    }
+};
+
+
+// two random numbers for the dice
+console.log(Math.floor(1 + Math.random() * 6));
+console.log(Math.floor(1 + Math.random() * 6));
+
+// random number to select score from array
+
+
+*/
